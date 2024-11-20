60ca4478d222fa46d02b56e59b631736db0ee876
ShabanHashim381 <shabantararr@gmail.com>
2024-11-20 23:11:28 +0500

    index.html

---
+++ modules1.mjs
@@ -0,0 +1,4 @@
+import dza, { a, c, d } from "./modules2.mjs";
+console.log(dza);
+console.log(c);
+console.log(d);
---
+++ modules2.mjs
@@ -0,0 +1,10 @@
+const a = "Shaban";
+const b = "Ali";
+const c = "asif";
+const d = "omer";
+
+export default b;
+
+export { a };
+export { c };
+export { d };
