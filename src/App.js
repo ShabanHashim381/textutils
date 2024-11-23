5d63ec4a787f795dbc31426b8ce7d491b016de5e
ShabanHashim381 <shabantararr@gmail.com>
2024-11-23 23:24:58 +0500

    App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
+import About from "./Components/About";
 import Navbar from "./Components/Navbar";
-import TextForm from "./Components/TextForm";
+// import TextForm from "./Components/TextForm";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       {/* <Navbar /> */}
       <Navbar title="Textutils" />
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze" />
+        {/* <TextForm heading="Enter the text to analyze" /> */}
+        <About />
       </div>
     </>
   );
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -2,8 +2,16 @@ import React, { useState } from "react";
 
 export default function TextForm(props) {
   const handleUpClick = () => {
-    console.log("Uppercase was Clicked" + text);
+    // console.log("Uppercase was Clicked" + text);
     let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+  const handleClearClick = () => {
+    let newText = "";
     setText(newText);
   };
   const handleOnChange = (event) => {
@@ -14,20 +22,37 @@ export default function TextForm(props) {
   //   text="new text" // wrong way to change the state
   //   setText=("new text") // correct way to change the state
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="myBox"
-          rows="8"
-        ></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLoClick}>
+          Convert to Lowercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleClearClick}>
+          Clear Text
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
-        Convert to Uppercase
-      </button>
-    </div>
+      <div className="container my-3">
+        <h1>Your text summary</h1>
+        <p>
+          {text.split(" ").length} words {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
